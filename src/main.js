35516ff3f17a6d32f7c1a9cb8373bc69ffbfc298
Ehsan Zargar-Ershadi <ershadi.ehsan@gmail.com>
2018-04-20 11:46:56 +0100

    Create a simple menu

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log('hi');
+var Electron = require('electron');
+var App = Electron.app;
+var Win = Electron.BrowserWindow;
+var Menu = Electron.Menu;
+
+
+App.on('ready', _ => {
+    new Win();
+
+    var template = [
+        {
+            label: "ddd",
+        }
+    ];
+
+    var menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+});
