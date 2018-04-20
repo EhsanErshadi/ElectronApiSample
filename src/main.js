2fb893d737f7d46accf98867e9243ba14c4718ed
Ehsan Zargar-Ershadi <ershadi.ehsan@gmail.com>
2018-04-20 12:41:08 +0100

    Generate Guid By Ctrl+G

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-const { electron, app, globalShortcut, BrowserWindow, Tray, Menu } = require('electron')
+const { electron, app, globalShortcut, BrowserWindow, Tray, Menu, clipboard } = require('electron')
 const path = require('path');
+const uuidv1 = require('uuid/v1');
+
+let tray;
 
 app.on('ready', _ => {
     new BrowserWindow();
 
     globalShortcut.register('CommandOrControl+Q', () => {
         app.quit();
+    });
+    globalShortcut.register('CommandOrControl+G', () => {
+        GenerateGuid();
     })
 
     var template = [
@@ -36,12 +42,27 @@ app.on('ready', _ => {
     Menu.setApplicationMenu(menu);
 
 
-    var tray = new Tray(path.join('assets/image', 'app-512.png'));
+    tray = new Tray(path.join('assets/image', 'app-512.png'));
     var trayMenu = Menu.buildFromTemplate([
         {
             label: 'Wow',
-            click: e => { console.log('hi') }
+            click: e => { GenerateGuid(); }
         }
     ]);
     tray.setContextMenu(trayMenu);
+
+    tray.setToolTip('ok, click me ...');
+
+
 });
+
+function GenerateGuid() {
+    var guid = uuidv1().toUpperCase();
+    console.log('Guid Generated: ' + guid);
+    clipboard.writeText(guid);
+
+    tray.displayBalloon({
+        title: 'Guid is in clipboard',
+        content: guid
+    });
+}
